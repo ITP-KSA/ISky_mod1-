32740b76345da2d5c6a7ca2e2d0a698d38d55078
odoo <kchauhan@nishutatechnolabs.com>
2018-11-06 16:07:10 +0530

    [IMP]added filters in sale order

--- sale_purchase_customizations/models/purchase_order_line.py
+++ sale_purchase_customizations/models/purchase_order_line.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class PurchaseOrderLines(models.Model):
@@ -15,3 +15,23 @@ class PurchaseOrderLines(models.Model):
         string="SMACC Code", related="product_id.product_smacc_code")
     product_samj_code = fields.Char(
         string="SAMJ Code", related="product_id.product_samj_code")
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    READONLY_STATES = {
+        'purchase': [('readonly', True)],
+        'done': [('readonly', True)],
+        'cancel': [('readonly', True)],
+    }
+
+    @api.multi
+    def _set_default_picking_type(self):
+        return self.env['stock.picking.type']
+
+    picking_type_id = fields.Many2one(
+        'stock.picking.type', 'Deliver To',
+        states=READONLY_STATES, required=True,
+        default=_set_default_picking_type,
+        help="This will determine operation type of incoming shipment")
